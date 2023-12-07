c6d8e0088064b36fa0173c318f6666116cf87ac3
Saeed Ebrahimi <sebrahimi@berkeley.edu>
2023-12-07 04:27:14 +0000

    [test] Write Cucumber tests for ProPublica Campaign Finance API#26

---
+++ features/step_definitions/campaign_finances_steps.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Given('I navigate to the Campaign Finances page') do
+  visit '/campaignfinances'
+end
+
+
+
+When(/^I select "(.*)" from the "(.*)" dropdown$/) do |option, field|
+  select(option, from: field)
+end
+
+
+
+Then('I should see a table with campaign finance data') do
+  expect(page).to have_css('table#events')
+end
